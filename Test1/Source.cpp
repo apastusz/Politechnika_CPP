672744edd007c076c12a922f8cd47f1982d7185a
Adam Pastuszka <apastu@gmail.com>
2015-12-29 20:25:45 +0100

    Test_1 update

--- Test1/Source.cpp
+++ Test1/Source.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+
+string PIN;
 
 int main()
 {
-	cout << "dupa" << endl;
+	cout << "Witamy na stronach banku" << endl;
+	cout << "Wprowadz PIN: " << endl;
+	cin >> PIN;
+
+	if (PIN == "1111") {
+		cout << "Poprawny PIN"  << endl;
+	}
+	else {
+		cout << "Niepoprawny PIN" << endl;
+	}
 
 	system("pause");
 	return 0;
