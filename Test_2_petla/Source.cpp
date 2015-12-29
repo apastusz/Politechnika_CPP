91e05c82bd35cf7394bc4cafadba4e422e41470c
Adam Pastuszka <apastu@gmail.com>
2015-12-29 21:22:32 +0100

    Pętla_zmiana

---
+++ Test_2_petla/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	
+	for (int i = 10; i>= 0;i--)
+	{ 
+		Sleep(1000);
+		cout << i << endl;
+
+	}
+		Sleep(2000);
+		cout << "Odlot" << endl;
+		
+
+	system("pause");
+	return 0;
+
+
+}
